be4a97a4f66f4f13b23895989ee42d21e6789a93
RDenis19 <115793603+RDenis19@users.noreply.github.com>
2025-05-01 20:24:21 -0500

    Actualizar run2.py

--- ejemplo05/run2.py
+++ ejemplo05/run2.py
@@ -1,16 +1,16 @@
 import requests
 import json
 
-# Carga el JSON generado
+# Carga el json
 with open('atp_tennis.json', 'r') as f:
     data = json.load(f)
 
-# Nombre de la base CouchDB
+# Nombre de la base y configuración para enviar los datos
 base_datos = "personas0005"
 url = f"http://127.0.0.1:5984/{base_datos}"
 headers = {'Content-Type': 'application/json'}
 
-# Enviar todos los documentos (Json)
+# Enviar todos los documentos (Json) mediante docs
 for doc in data['docs']:
     response = requests.post(url, headers=headers, json=doc)
     # Imprime Player_1
